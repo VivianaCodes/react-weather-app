22e78d793a0972f16e0196269f687794d6d73d05
VivianaCodes <viviana.milone3@gmail.com>
2023-04-12 22:02:04 +0200

    Added Date component

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 
@@ -13,7 +14,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       name: response.data.name,
       description: response.data.weather[0].description,
-      date: "Monday 10.04 14:33",
+      date: new Date(response.data.dt * 1000),
       iconUrl:
         "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png",
     });
@@ -42,9 +43,11 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1> {weatherData.city}</h1>
+        <h1> {props.defaultCity}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
@@ -65,7 +68,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.deafultCity}&appid=d59265a17617259a858c34b2c6833049&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=d59265a17617259a858c34b2c6833049&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
