1fdfb8d57a562b865e3741d2e66604d1bcbff1e4
VivianaCodes <viviana.milone3@gmail.com>
2023-04-16 22:45:11 +0200

    added links to footer

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ function App() {
         <Weather defaultCity="Naples" />
         <footer>
           This project was coded by{" "}
-          <a href=" " target="_blank" rel="noreferrer">
+          <a
+            href="https://www.linkedin.com/in/viviana-milone-9149a56/ "
+            target="_blank"
+            rel="noreferrer"
+          >
             Viviana Milone
           </a>{" "}
           and is{" "}
-          <a href="" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/VivianaCodes/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
             open-sourced on Github
           </a>
         </footer>
