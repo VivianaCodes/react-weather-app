8bb1655abb4a330941f2d51f95e2d306b1928855
VivianaCodes <viviana.milone3@gmail.com>
2023-05-02 23:05:53 +0200

    added Api call for FOrecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./WeatherForecast.css";
 import WeatherIcon from "./WeatherIcon";
+import Axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  console.log(props);
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=64469ac67e6dc941feb5b50915a18dc7&units=metric`;
+  Axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
